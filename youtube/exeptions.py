0b247a6ae2f36c0bcd2141d1e56747b85dfa2da3
russarr <russarr@gmail.com>
2024-08-30 10:01:35 +0500

    feat: creating exceptions module

---
+++ youtube/exeptions.py
@@ -0,0 +1,4 @@
+"""
+googleapiclient.errors.HttpError: <HttpError 403 when requesting https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&id=aOly5eEDXug%2CDvdZv_DD0DY&alt=json returned "The request cannot be completed because you have exceeded your <a href="/youtube/v3/getting-started#quota">quota</a>.". Details: "[{'message': 'The request cannot be completed because you have exceeded your <a href="/youtube/v3/getting-started#quota">quota</a>.', 'domain': 'youtube.quota', 'reason': 'quotaExceeded'}]">
+"""
+#TODO: add exception to capture quotaExceeded

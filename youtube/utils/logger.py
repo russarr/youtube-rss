9c4147a7dc7e4a0cdfe1c339b2ceea831b7966da
russarr <russarr@gmail.com>
2024-09-15 10:12:33 +0500

    feat: update logger module

--- youtube/utils/logger.py
+++ youtube/utils/logger.py
@@ -8,18 +8,17 @@ from pathlib import Path
 from typing import Literal, override
 from urllib.error import HTTPError, URLError
 
-from youtube.utils.utils import load_project_config
+from config import env
 
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_COMMON_CHAT_ID")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-config = load_project_config()
 
-PROJECT_NAME: str = config.get("project_name", "Not set")
-LOG_FILE: str = config.get("log_file", "")
+PROJECT_NAME: str = env.PROJECT_NAME
+LOG_FILE: str = env.LOG_FILE
 
-DEBUG_MODE: bool = config.get("debug_mode", True)
+DEBUG_MODE: bool = env.DEBUG_MODE
 
+TELEGRAM_CHAT_ID: str = env.TELEGRAM_CHAT_ID
+TELEGRAM_BOT_TOKEN: str = env.TELEGRAM_BOT_TOKEN
 
 log_level_aliases = {
     "D": logging.DEBUG,
@@ -29,8 +28,40 @@ log_level_aliases = {
     "C": logging.CRITICAL,
 }
 
+def conf_logger(
+    logger_name: str,
+    log_level: Literal["C", "E", "W", "I", "D", "N"] = "E",
+    *,
+    capture_warnins: bool = False,  # noqa: FBT
+) -> Logger:
+    """Function to create configured_logger"""
 
-def add_project_name_attr(project_name: str) -> None:
+    logging.captureWarnings(capture=capture_warnins)
+
+    logger = logging.getLogger(logger_name)
+
+    _add_project_name_attr(PROJECT_NAME)
+
+    logger.addHandler(_create_console_handler())
+    if DEBUG_MODE:
+        level = log_level_aliases[log_level]
+    else:
+        level = logging.ERROR
+
+        if TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID:
+            logger.addHandler(
+                _create_telegram_handler(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID),
+            )
+
+        if LOG_FILE:
+            _create_log_directory(LOG_FILE)
+            logger.addHandler(_create_file_handler())
+
+    logger.setLevel(level)
+
+    return logger
+
+def _add_project_name_attr(project_name: str) -> None:
     """Function add project name to messages"""
 
     old_factory = logging.getLogRecordFactory()
@@ -49,10 +80,7 @@ def add_project_name_attr(project_name: str) -> None:
     )
 
 
-add_project_name_attr(PROJECT_NAME)
-
-
-def create_console_handler() -> Handler:
+def _create_console_handler() -> Handler:
     """Function to create console handler for logging"""
     formatter = logging.Formatter(
         fmt='[{levelname:<8}] [{name}:{funcName}:{lineno}]: "{message}"',
@@ -66,7 +94,7 @@ def create_console_handler() -> Handler:
     return handler
 
 
-def create_file_handler() -> Handler:
+def _create_file_handler() -> Handler:
     """Function to create file handler for logging"""
     formatter = logging.Formatter(
         fmt='[{levelname:<8}: {asctime}] [{name}:{funcName}:{lineno}]: "{message}"',
@@ -79,7 +107,7 @@ def create_file_handler() -> Handler:
     return handler
 
 
-def send_telegram_message(text: str) -> None:
+def _send_telegram_message(text: str) -> None:
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendmessage"
     post_data = {"chat_id": TELEGRAM_CHAT_ID, "text": text}
     data = urllib.parse.urlencode(post_data).encode()
@@ -100,10 +128,10 @@ class TelegramBotHandler(Handler):
 
     @override
     def emit(self, record: LogRecord) -> None:
-        send_telegram_message(text=self.format(record))
+        _send_telegram_message(text=self.format(record))
 
 
-def create_telegram_handler(token: str, chat_id: str) -> Handler:
+def _create_telegram_handler(token: str, chat_id: str) -> Handler:
     """Function to create file handler for logging"""
     formatter = logging.Formatter(
         fmt='[{levelname:<8}: {asctime}] [project: {project_name}] \
@@ -117,35 +145,7 @@ def create_telegram_handler(token: str, chat_id: str) -> Handler:
     return handler
 
 
-def create_log_directory(LOG_FILE: str) -> None:  # noqa: N803
+def _create_log_directory(LOG_FILE: str) -> None:  # noqa: N803
     Path(LOG_FILE).parent.mkdir(parents=True, exist_ok=True)
 
 
-def conf_logger(
-    logger_name: str,
-    log_level: Literal["C", "E", "W", "I", "D", "N"] = "E",
-    *,
-    capture_warnins: bool = False,  # noqa: FBT
-) -> Logger:
-    """Function to create configured_logger"""
-
-    logging.captureWarnings(capture=capture_warnins)
-
-    logger = logging.getLogger(logger_name)
-
-    logger.addHandler(create_console_handler())
-    if DEBUG_MODE:
-        level = log_level_aliases[log_level]
-    else:
-        level = logging.ERROR
-        if TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID:
-
-            logger.addHandler(
-                create_telegram_handler(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID),
-            )
-        if LOG_FILE:
-            create_log_directory(LOG_FILE)
-            logger.addHandler(create_file_handler())
-    logger.setLevel(level)
-
-    return logger

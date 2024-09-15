92c6cb36a3e4b3d28bfccb6eadf63d39acac9bce
russarr <russarr@gmail.com>
2024-09-15 10:12:14 +0500

    feat: new todo exception validation error

--- youtube/exeptions.py
+++ youtube/exeptions.py
@@ -5,6 +5,14 @@ googleapiclient.errors.HttpError: <HttpError 403 when requesting https://youtube
 # TODO: add exception to capture quotaExceeded
 
 
+"""pydantic_core._pydantic_core.ValidationError: 1 validation error for VideosResponse
+items.0.contentDetails.duration
+  Field required [type=missing, input_value={'dimension': '2d', 'defi...jection': 'rectangular'}, input_type=dict]
+    For further information visit https://errors.pydantic.dev/2.8/v/missing"""
+
+#TODO: check validateion
+
+
 class SettingsError(Exception):
     """Exeptions for wrong argumets in youtube resource function"""
 
